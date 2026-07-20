b36ee726ad2467e2559b6c20271d6e34016b9e8e
Andre R. Rockwell <anrd.rockwell@gmail.com>
2026-07-20 00:14:44 -0400

    fix: restore roc-terraform prefix in naming convention

--- main.tf
+++ main.tf
@@ -87,8 +87,8 @@ locals {
   availability_zone_2 = data.aws_availability_zones.available.names[1]
   ami_id              = data.aws_ami.amazon_linux.id
 
-  # Naming convention: {env}-{project}-{resource}-{suffix}
-  name_prefix  = "${var.environment}-${var.project}"
-  short_prefix = "${var.environment}-${substr(var.project, 0, 4)}"
+  # Naming convention: {env}-roc-terraform-{project}-{resource}-{suffix}
+  name_prefix  = "${var.environment}-roc-terraform-${var.project}"
+  short_prefix = "${var.environment}-roc-${substr(var.project, 0, 2)}"
   suffix       = random_string.suffix.result
 }
